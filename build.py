c6651ed568a92c3077d3bcf50fdb7795367f3947
180031019satyaprasad <180031019@kluniversity.in>
2021-01-20 12:22:27 +0530

    first commit

---
+++ build.py
@@ -0,0 +1,10 @@
+#python program for adding two numbers
+
+num1 = 1.5
+num2 = 6.3
+
+#add two numbers
+sum = num1+num2
+
+#display the sum 
+print('the sum of {0} and {1} is {2}'. format (num1,num2,sum))oad-a-project-to-github
