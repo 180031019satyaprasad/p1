7cdd9982a6c827340be0d08b81a65985b73b0704
180031019satyaprasad <76677063+180031019satyaprasad@users.noreply.github.com>
2021-01-27 14:14:22 +0530

    Update build.py

--- build.py
+++ build.py
@@ -3,8 +3,4 @@
 num1 = 1.5
 num2 = 6.3
 
-#add two numbers
-sum = num1+num2
-
-#display the sum 
-print('the sum of {0} and {1} is {2}'. format (num1,num2,sum))oad-a-project-to-github
+print( num1 + num2)
